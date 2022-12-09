c34f34562b7ea4dd5f71aced45acf4658ec89099
darjusch <darjusch.schrand@code.berlin>
2022-12-09 18:51:38 +0100

    read / write

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http');
-
+var express = require('express');
+var app = express();
+var fs = require('fs'),
+var url = require('url');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -7,6 +10,28 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
+
+app.post('/write', function(req, res) {
+	fs.writeFile('creds.txt', req.body, { flag: 'a+' }, err => {
+		if (err) {
+    			console.error(err);
+			res.send(err);  		
+}
+  		// file written successfully
+	});
+	res.send('Success!');
+})
+app.get('/read', function(req, res) {
+	fs.readFile('creds.txt', 'utf-8', (err, data ) => {
+	if(err) {
+	console.error(err);
+	res.send(err);
+	}
+	console.log(data);
+	res.send(data);
+	}
+})
+
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
