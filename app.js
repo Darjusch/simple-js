b3247d269f035f066e13aed1c1d7a9c67b6f6946
darjusch <darjusch.schrand@code.berlin>
2022-12-09 21:57:29 +0100

    added dependency and fixed bracket

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 var express = require('express');
 var app = express();
-var fs = require('fs')
+var fs = require('fs');
 var url = require('url');
 const port = 3000;
 
@@ -11,27 +11,25 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-app.post('/write', function(req, res) {
-	fs.writeFile('creds.txt', req.body, { flag: 'a+' }, err => {
-		if (err) {
-    			console.error(err);
-			res.send(err);  		
-}
-  		// file written successfully
-	});
-	res.send('Success!');
-})
-app.get('/read', function(req, res) {
-	fs.readFile('creds.txt', 'utf-8', (err, data ) => {
-	if(err) {
-	console.error(err);
-	res.send(err);
-	}
-	console.log(data);
-	res.send(data);
-	}
-})
-})
+app.post('/write', function (req, res) {
+  fs.writeFile('creds.txt', req.body, { flag: 'a+' }, (err) => {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    }
+  });
+  res.send('Success!');
+});
+app.get('/read', function (req, res) {
+  fs.readFile('creds.txt', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    }
+    console.log(data);
+    res.send(data);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
